b4fc3de603adc55ceebeea30eeb554a29faaaaa4
Looganesh <mohankunju59@gmail.com>
2025-08-21 04:16:06 +0800

    added num of players input and draw two cards for each player

--- Poker.py
+++ Poker.py
@@ -1,17 +1,31 @@
 import random
-
-def Poker ():
+# First Function for Input Players Names
+def main ():
+    players = []
+    while True:
+        player = (input("Enter the Name of Players (E TO EXIT): ")).upper()
+        if player == "E":
+            break
+        players.append(player)
+    Poker(players)
+                        
+#  Second Function Draw 2 Cards for Each Player       
+def Poker (Nplayer):
     
     deck = ["Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts",
  "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades",
  "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds",
  "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs"]
-    
-    #shuffle the decks and store it
+
+# Shuffle the Deck and Make a Copy   
     random.shuffle(deck)
     shuffled_deck = deck.copy()
-    print(shuffled_deck)
-    draw = random.choices(shuffled_deck, k=2)
-    print("draw :" ,draw )
-    
-Poker()
+
+# Draw 2 Cards From the Shuffled Deck     
+    for n in Nplayer :
+        draw = random.choices(shuffled_deck, k=2)
+        print("Player : ", n)
+        print("Cards : ", draw)
+        print("")
+
+main()
