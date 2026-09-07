bf83d04b5b3b4438411c44add182f47cd70cfb01
Looganesh <mohankunju59@gmail.com>
2026-09-07 16:20:36 +0800

    fix invalid number are not attempts

--- Numbers Guessing.py
+++ Numbers Guessing.py
@@ -6,16 +6,17 @@ attempts = 0
 
 while attempts < 7 :
     guess = int(input("enter a number: "))
-    attempts += 1
     if guess < 1 :
-        print("invaild number")
-    elif guess > secrets :
-        print("too high")
-    elif guess < secrets :
-        print("too low")
+        print("invaild number try again")
     else:
-        print("you got, nice work!!!")
-        break
+        attempts += 1
+        if guess > secrets :
+            print("too high")
+        elif guess < secrets :
+            print("too low")
+        else:
+            print("you got, nice work!!!")
+            break
 
 if guess != secrets:
     print(f"you lose, maximum tries reached {attempts}")
